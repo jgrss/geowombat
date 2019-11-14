aab5888b27081b5de57ec4c7d1934ab5db8027c5
Jordan <graesserj@athena52.cm.cluster>
2019-11-14 16:08:02 +1000

    added c++ CRF feature transformer

--- geowombat/models/_crf.pyx
+++ geowombat/models/_crf.pyx
@@ -1,6 +1,5 @@
 # distutils: language = c++
 # cython: profile=False
-# cython: cdivision=True
 # cython: boundscheck=False
 # cython: wraparound=False
 
@@ -18,74 +17,56 @@ DTYPE_float64 = np.float64
 ctypedef np.float64_t DTYPE_float64_t
 
 
-cdef cpp_map[string, double] _sample_to_dict(double[::1] array_sample,
-                                             unsigned int nfeas,
-                                             vector[string] band_names_):
+cdef cpp_map[string, double] _sample_to_dict(double[::1] tsamp):
 
     """
     Converts names and a 1d array to a dictionary
     """
 
     cdef:
-        Py_ssize_t k
-        cpp_map[string, double] features_map_sub
+        Py_ssize_t t
+        unsigned int tsamp_len = tsamp.shape[0]
+        cpp_map[string, double] features_map
 
-    for k in range(0, nfeas):
-        features_map_sub[band_names_[k]] = array_sample[k]
+    for t in range(1, tsamp_len+1):
+        features_map[str(t).encode('utf-8')] = tsamp[t-1] * 0.0001
 
-    return features_map_sub
+    return features_map
 
 
-def time_to_crffeas(cpp_map[int, double[:, ::1]] features_map,
-                    vector[char] band_names,
-                    unsigned int nfeas):
+def time_to_crffeas(double[:, :, ::1] data,
+                    unsigned int ntime,
+                    unsigned int nrows,
+                    unsigned int ncols):
 
     """
     Converts a time-shaped array to CRF features
 
     Args:
-        data (4d array): time x bands x rows x columns.
-
-    Example:
-        time_to_features(array, ['swir1', 'swir2', 'date-diff', 'b-b'])
+        data (list): Vector of length 'time'.
 
     Returns:
         ``list`` of feature dictionaries
     """
 
-    # cdef:
-    #     Py_ssize_t ti, si, i, j, m
-        # unsigned int ntime = data.shape[0]
-        # unsigned int nbands = data.shape[1]
-        # unsigned int nrows = data.shape[2]
-        # unsigned int ncols = data.shape[3]
-        # unsigned int nsamps = <int>(nrows * ncols)
-        # np.ndarray[double, ndim=3] tlayer
-        # cpp_map[int, double[:, ::1]] features_map
-        # vector[double[::1]] feature_vector_temp
-        # vector[cpp_map[string, double]] feature_vector
-        # unsigned int nfeas = len(band_names)
-        # vector[char] band_names_
+    cdef:
+        Py_ssize_t i, j
+        double[:, ::1] tdata
+        double[::1] tsample
+        vector[cpp_map[string, double]] samples
+        vector[vector[cpp_map[string, double]]] samples_full
 
-    # for si in range(0, nfeas):
-    #     band_names_.push_back(band_names[si].encode('utf8'))
+    for i in range(0, nrows*ncols):
 
-    print(band_names)
+        for j in range(0, ntime):
 
-    # for ti in range(0, ntime):
-    #
-    #     tlayer = data[<int>ti]
-    #     features_map[<int>ti] = np.ascontiguousarray(tlayer.transpose(1, 2, 0).reshape(nsamps, nbands), dtype='float64')
-    print(features_map)
-    # for i in range(0, nsamps):
-    #
-    #     for j in range(0, ntime):
-    #         feature_vector_temp.push_back(features_map[j][i])
-    #
-    # for m in range(0, nsamps*ntime):
-    #
-    #     feature_vector.push_back(_sample_to_dict(feature_vector_temp[m],
-    #                                              nfeas,
-    #                                              band_names_))
-    #
-    # return feature_vector
+            tdata = data[j]
+            tsample = tdata[i, :]
+
+            samples.push_back(_sample_to_dict(tsample))
+
+        samples_full.push_back(samples)
+
+        samples.clear()
+
+    return samples_full
