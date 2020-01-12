869654788fc7d4074767564ad73362d5e6e4a7b2
jgrss <graesser@bu.edu>
2020-01-12 17:49:34 +1000

    added additional pan DataArray user argument

--- geowombat/radiometry/sharpen.py
+++ geowombat/radiometry/sharpen.py
@@ -3,10 +3,10 @@ import xarray as xr
 
 def pan_sharpen(data,
                 bands=None,
+                pan=None,
                 blue_weight=0.2,
                 green_weight=1.0,
                 red_weight=1.0,
-                nir_weight=1.0,
                 scale_factor=1.0):
 
     """
@@ -14,11 +14,12 @@ def pan_sharpen(data,
 
     Args:
         data (DataArray): The band data.
-        bands (Optional[list]): The bands to sharpen. If not given, 'blue', 'green', 'red', and 'nir' are used.
+        bands (Optional[list]): The bands to sharpen. If not given, 'blue', 'green', and 'red' are used.
+        pan (Optional[DataArray]): The panchromatic ``DataArray``. ``pan`` is only needed if it is not included
+            with ``data``.
         blue_weight (Optional[float]): The blue band weight.
         green_weight (Optional[float]): The green band weight.
         red_weight (Optional[float]): The red band weight.
-        nir_weight (Optional[float]): The NIR band weight.
         scale_factor (Optional[float]): A scale factor to apply to the data.
 
     Example:
@@ -43,9 +44,14 @@ def pan_sharpen(data,
 
     data = data * scale_factor
 
-    dnf = data.sel(band='pan') / ((data.sel(band='blue') * blue_weight +
-                                   data.sel(band='green') * green_weight +
-                                   data.sel(band='red') * red_weight) / (blue_weight + green_weight + red_weight))
+    if isinstance(pan, xr.DataArray):
+        pan = pan.sel(band='pan') * scale_factor
+    else:
+        pan = data.sel(band='pan')
+
+    dnf = pan / ((data.sel(band='blue') * blue_weight +
+                  data.sel(band='green') * green_weight +
+                  data.sel(band='red') * red_weight) / (blue_weight + green_weight + red_weight))
 
     dnf = dnf.assign_coords(coords={'band': 'pan'})
     dnf = dnf.expand_dims(dim='band')
