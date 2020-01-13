37cd37bcb7ee09bc50d60609466cb84c4cd5881e
jgrss <graesser@bu.edu>
2020-01-13 20:59:13 +1000

    updated readthedocs

--- build/lib.linux-x86_64-3.6/geowombat/backends/__init__.py
+++
@@ -1,8 +0,0 @@
-from .dask_ import Cluster
-from .xarray_ import concat, mosaic
-from .xarray_ import warp_open
-
-__all__ = ['Cluster',
-           'concat',
-           'mosaic',
-           'warp_open']
--- build/lib.linux-x86_64-3.6/geowombat/util/__init__.py
+++
@@ -1,6 +0,0 @@
-from .plotting import Plotting
-from .web import GeoDownloads
-
-imshow = Plotting().imshow
-
-__all__ = ['imshow', 'GeoDownloads']
