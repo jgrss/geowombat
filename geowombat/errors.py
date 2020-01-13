400aa18849868a04f9ee17f313f8b4098419ce53
jgrss <graesser@bu.edu>
2020-01-13 18:41:28 +1000

    added file logging

--- geowombat/errors.py
+++ geowombat/errors.py
@@ -1,3 +1,4 @@
+import os
 import logging
 
 _FORMAT = '%(asctime)s:%(levelname)s:%(lineno)s:%(module)s.%(funcName)s:%(message)s'
@@ -5,7 +6,9 @@ _formatter = logging.Formatter(_FORMAT, '%H:%M:%S')
 _handler = logging.StreamHandler()
 _handler.setFormatter(_formatter)
 
-logging.basicConfig(level=logging.DEBUG)
+logging.basicConfig(filename=os.path.join(os.path.abspath(os.path.dirname(__file__)), 'geowombat.log'),
+                    filemode='w',
+                    level=logging.DEBUG)
 
 logger = logging.getLogger(__name__)
 logger.addHandler(_handler)
