806ededc6643b444878864e299b4a471425604ec
Jordan <graesserj@athena52.cm.cluster>
2020-02-13 11:41:25 +1000

    added check for writeable directory

--- geowombat/errors.py
+++ geowombat/errors.py
@@ -7,7 +7,14 @@ _formatter = logging.Formatter(_FORMAT, '%H:%M:%S')
 _handler = logging.StreamHandler()
 _handler.setFormatter(_formatter)
 
-logging.basicConfig(filename=os.path.join(os.path.abspath(os.path.dirname(__file__)), 'geowombat.log'),
+_log_home = os.path.abspath(os.path.dirname(__file__))
+
+if not os.access(_log_home, os.W_OK):
+    _log_home = os.path.expanduser('~')
+
+_log_file = os.path.join(_log_home, 'geowombat.log')
+
+logging.basicConfig(filename=_log_file,
                     filemode='w',
                     level=logging.DEBUG)
 
