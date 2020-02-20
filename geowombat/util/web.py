320ddf8479b7e9d81540afa67bb817850bb32cc9
jgrss <graesser@bu.edu>
2020-02-20 22:22:39 +1000

    removed redundant argument

--- geowombat/util/web.py
+++ geowombat/util/web.py
@@ -132,7 +132,6 @@ class GeoDownloads(object):
                       date_range,
                       bounds,
                       bands,
-                      mask_qa=False,
                       crs=None,
                       out_bounds=None,
                       outdir='.',
@@ -159,7 +158,6 @@ class GeoDownloads(object):
                     Sentinel s2cloudless bands:
                         bands = ['coastal', 'blue', 'red', 'nir1', 'nir', 'rededge', 'water', 'cirrus', 'swir1', 'swir2']
 
-            mask_qa (Optional[bool]): Whether to mask QA.
             crs (Optional[str or object]): The output CRS. If ``bounds`` is a ``GeoDataFrame``, the CRS is taken
                 from the object.
             out_bounds (Optional[list or tuple]): The output bounds in ``crs``. If not given, the bounds are
