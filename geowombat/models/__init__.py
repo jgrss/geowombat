4549592327ccefe3679f2571755303216325a6f7
jgrss <graesser@bu.edu>
2020-08-07 06:36:40 +1000

    removed old imports

--- geowombat/models/__init__.py
+++ geowombat/models/__init__.py
@@ -1,17 +1,10 @@
-from ._crf import labels_to_values
-from ._crf import time_to_feas
-from ._crf import time_to_sensor_feas
-from ._crf import transform_probas
 from .model import CRFClassifier
 from .model import GeoWombatClassifier
 from .model import Predict
+
 
 predict = Predict().predict
 
 __all__ = ['CRFClassifier',
            'GeoWombatClassifier',
-           'predict',
-           'labels_to_values',
-           'time_to_feas',
-           'time_to_sensor_feas',
-           'transform_probas']
+           'predict']
