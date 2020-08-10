eda8016ab3b4c9a8f40eda3701bfb7a4967c116a
jgrss <graesser@bu.edu>
2020-08-10 14:29:49 +1000

    reorganized sixs class

--- geowombat/radiometry/sixs.py
+++ geowombat/radiometry/sixs.py
@@ -29,7 +29,7 @@ import joblib
 logger = logging.getLogger(__name__)
 logger = add_handler(logger)
 
-LUTNames = namedtuple('LUTNames', 'base lut name zip')
+LUTNames = namedtuple('LUTNames', 'name path')
 
 p = Path(os.path.abspath(os.path.dirname(__file__)))
 DATA_PATH = p / '../data'
@@ -37,38 +37,10 @@ DATA_PATH = p / '../data'
 
 def _set_names(sensor_name):
 
-    s_base = DATA_PATH / 'lut' / sensor_name
-    s_lut = DATA_PATH / 'lut' / sensor_name / sensor_name / 'Continental' / 'view_zenith_0'
-    s_zip = DATA_PATH / 'lut' / '{}.zip'.format(sensor_name)
+    lut_path = DATA_PATH / 'lut' / '{}.bz2'.format(sensor_name)
 
-    return LUTNames(base=s_base,
-                    lut=s_lut,
-                    name=sensor_name,
-                    zip=s_zip)
-
-
-def interp_lut(sensors, bands):
-
-    """
-    Interpolates a list of sensors and bands
-
-    Args:
-        sensors (list): A list of sensors.
-        bands (list): A list of bands.
-
-    Returns:
-        ``None``
-
-    Example:
-        >>> from geowombat.radiometry.lut import interp_lut
-        >>>
-        >>> interp_lut(['l8'], [2, 3, 4, 5])
-    """
-
-    for sensor in sensors:
-        for band in bands:
-            sxs = SixS(sensor, band, verbose=1)
-            sxs.load()
+    return LUTNames(name=sensor_name,
+                    path=lut_path)
 
 
 class SixS(object):
@@ -85,55 +57,41 @@ class SixS(object):
         verbose (Optional[int]): The verbosity level.
 
     Example:
-        >>> ilut = InterpLUT('l5', 1, verbose=1)
-        >>> ilut.load()
+        >>> sixs = SixS('l5', verbose=1)
         >>>
-        >>> with gw.open('image.tif') as src:
-        >>>     ilut.rad_to_sr(src.sel(band=1), sza, doy, h2o=1.0, o3=0.4, aot=0.3)
+        >>> with gw.config.update(sensor='l7'):
+        >>>     with gw.open('image.tif') as src, gw.open('solar_za') as sza:
+        >>>         sixs.rad_to_sr(src, 'blue', sza, doy, h2o=1.0, o3=0.4, aot=0.3)
     """
 
-    def __init__(self, sensor, band, rad_scale=1.0, angle_scale=0.01, verbose=0):
+    def __init__(self, sensor, rad_scale=1.0, angle_scale=0.01, verbose=0):
 
         self.sensor = sensor
-        self.band = band
         self.rad_scale = rad_scale
         self.angle_scale = angle_scale
         self.verbose = verbose
+
         self.lut = None
 
-        self.sensor_lookup = {'l5': _set_names('LANDSAT_TM'),
-                              'l7': _set_names('LANDSAT_ETM'),
-                              'l8': _set_names('LANDSAT_OLI'),
-                              's2': _set_names('S2A_MSI')}
+        self.sensor_lookup = {'l5': _set_names('l5'),
+                              'l7': _set_names('l7'),
+                              'l8': _set_names('l8'),
+                              's2': _set_names('s2')}
 
-        if sensor.lower() == 's2':
+        # Load the lookup tables
+        self._load()
 
-            lut_format = '{NAME}_B{BAND:02d}.lut'.format(NAME=self.sensor_lookup[self.sensor].name, BAND=self.band)
-            ilut_format = '{NAME}_B{BAND:02d}.ilut'.format(NAME=self.sensor_lookup[self.sensor].name, BAND=self.band)
+    def _load(self):
+        self.lut = joblib.load(str(self.sensor_lookup[self.sensor].path))
 
-        else:
-
-            lut_format = '{NAME}_B{BAND:d}.lut'.format(NAME=self.sensor_lookup[self.sensor].name, BAND=self.band)
-            ilut_format = '{NAME}_B{BAND:d}.ilut'.format(NAME=self.sensor_lookup[self.sensor].name, BAND=self.band)
-
-        self.lut_path = self.sensor_lookup[self.sensor].lut / lut_format
-        self.ilut_path = self.sensor_lookup[self.sensor].lut / ilut_format
-
-        self._setup()
-
-        if not self.ilut_path.is_file():
-            self.interpolate()
-
-    def load(self, which='ilut'):
-        self.lut = self._load(which)
-
-    def rad_to_sr(self, data, sza, doy, h2o=1.0, o3=0.4, aot=0.3):
+    def rad_to_sr(self, data, band, sza, doy, h2o=1.0, o3=0.4, aot=0.3):
 
         """
         Gets 6s coefficients
 
         Args:
             data (DataArray): The data to correct, in radiance.
+            band (str): The band wavelength to process.
             sza (float | DataArray): The solar zenith angle.
             doy (int): The day of year.
             h2o (Optional[float]): The water vapor (g/m^2). [0,8.5].
@@ -148,8 +106,14 @@ class SixS(object):
             logger.exception('  The lookup table does not exist. Be sure to load it.')
             raise NameError
 
+        if band not in self.lut:
+            logger.exception('  The band {} does not exist in the LUT.'.format(band))
+
+        band_interp = self.lut[band]
+
         attrs = data.attrs.copy()
-        band_name = data.band.values
+
+        band_data = data.sel(band=band)
 
         altitude = 0.0
 
@@ -176,10 +140,10 @@ class SixS(object):
             valid_idx = np.where(~np.isnan(aot))
             aot = aot[valid_idx]
 
-        ab = self.lut(sza*self.angle_scale, h2o, o3, aot, altitude)
+        ab = band_interp(sza*self.angle_scale, h2o, o3, aot, altitude)
 
-        a = np.zeros(data.gw.nrows*data.gw.ncols, dtype='float64')
-        b = np.zeros(data.gw.nrows*data.gw.ncols, dtype='float64')
+        a = np.zeros(band_data.gw.nrows*band_data.gw.ncols, dtype='float64')
+        b = np.zeros(band_data.gw.nrows*band_data.gw.ncols, dtype='float64')
 
         a[:] = np.nan
         b[:] = np.nan
@@ -191,68 +155,45 @@ class SixS(object):
         a *= elliptical_orbit_correction
         b *= elliptical_orbit_correction
 
-        a = a.reshape(data.gw.nrows, data.gw.ncols)
-        b = b.reshape(data.gw.nrows, data.gw.ncols)
+        a = a.reshape(band_data.gw.nrows, band_data.gw.ncols)
+        b = b.reshape(band_data.gw.nrows, band_data.gw.ncols)
 
-        a = ndarray_to_xarray(data, a, ['coeff'])
-        b = ndarray_to_xarray(data, b, ['coeff'])
+        a = ndarray_to_xarray(band_data, a, ['coeff'])
+        b = ndarray_to_xarray(band_data, b, ['coeff'])
 
-        return ((data*self.rad_scale - a.sel(band='coeff')) / b.sel(band='coeff'))\
+        return ((band_data*self.rad_scale - a.sel(band='coeff')) / b.sel(band='coeff'))\
                     .expand_dims(dim='band')\
-                    .assign_coords(coords={'band': [band_name]})\
+                    .assign_coords(coords={'band': [band]})\
                     .assign_attrs(**attrs)
 
-    def _setup(self):
-
-        if not (DATA_PATH / 'lut.tar.gz').is_dir():
-
-            with tarfile.open(DATA_PATH / 'lut.tar.gz', mode='r:gz') as tf:
-                tf.extractall(path=DATA_PATH)
-
-        if not self.sensor_lookup[self.sensor].base.is_dir():
-
-            with zipfile.ZipFile(self.sensor_lookup[self.sensor].zip, mode='r') as uzip:
-                uzip.extractall(path=self.sensor_lookup[self.sensor].base)
-
-    def _load(self, which):
-
-        lut_path_ = self.lut_path if which == 'lut' else self.ilut_path
-
-        return joblib.load(lut_path_)
-
-    def _dump(self, interp_obj):
-
-        with open(self.ilut_path, 'wb') as lut_file:
-            pickle.dump(interp_obj, lut_file)
-
-    def interpolate(self):
-
-        """
-        Interpolates look-up tables
-        """
-
-        if self.verbose > 0:
-            logger.info('  Interpolating the LUT ...')
-
-        lut_dict = self._load('lut')
-
-        in_vars = lut_dict['config']['invars']
-
-        # all permutations
-        inputs = list(itertools.product(in_vars['solar_zs'],
-                                        in_vars['H2Os'],
-                                        in_vars['O3s'],
-                                        in_vars['AOTs'],
-                                        in_vars['alts']))
-
-        # output variables (6S correction coefficients)
-        outputs = lut_dict['outputs']
-
-        interpolator = LinearNDInterpolator(inputs, outputs)
-
-        self._dump(interpolator)
-
-        if self.verbose > 0:
-
-            logger.info('  Finished interpolating for sensor {SENSOR}, band {BAND}.'.format(SENSOR=self.sensor,
-                                                                                            BAND=self.band))
+    # def interpolate(self):
+    #
+    #     """
+    #     Interpolates look-up tables
+    #     """
+    #
+    #     if self.verbose > 0:
+    #         logger.info('  Interpolating the LUT ...')
+    #
+    #     lut_dict = self._load('lut')
+    #
+    #     in_vars = lut_dict['config']['invars']
+    #
+    #     # all permutations
+    #     inputs = list(itertools.product(in_vars['solar_zs'],
+    #                                     in_vars['H2Os'],
+    #                                     in_vars['O3s'],
+    #                                     in_vars['AOTs'],
+    #                                     in_vars['alts']))
+    #
+    #     # output variables (6S correction coefficients)
+    #     outputs = lut_dict['outputs']
+    #
+    #     interpolator = LinearNDInterpolator(inputs, outputs)
+    #
+    #     self._dump(interpolator)
+    #
+    #     if self.verbose > 0:
+    #
+    #         logger.info('  Finished interpolating for sensor {SENSOR}, band {BAND}.'.format(SENSOR=self.sensor,
+    #                                                                                         BAND=self.band))
