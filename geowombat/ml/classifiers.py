320b658cd50003a1b9c276f1f934ecd090738aae
jgrss <graesser@bu.edu>
2020-08-15 20:05:31 +1000

    added arguments

--- geowombat/ml/classifiers.py
+++ geowombat/ml/classifiers.py
@@ -1,12 +1,41 @@
 from .. import polygon_to_array
 
 import xarray as xr
+from sklearn_xarray import Target
+from sklearn_xarray.model_selection import CrossValidatorWrapper
+from sklearn.model_selection import GridSearchCV, GroupShuffleSplit
+from sklearn.preprocessing import LabelEncoder
 
 
-class Classifiers(object):
+class ClassifiersMixin(object):
 
     @staticmethod
-    def fit(data, labels, col=None):
+    def grid_search_cv(pipeline):
+
+        # TODO: groupby arg
+        cv = CrossValidatorWrapper(GroupShuffleSplit(n_splits=1,
+                                                     test_size=0.5),
+                                   groupby=['time'])
+
+        # TODO: param_grid arg
+        clf = GridSearchCV(pipeline,
+                           cv=cv,
+                           verbose=1,
+                           param_grid={"pca__n_components": [5]})
+
+        return clf
+
+
+class Classifiers(ClassifiersMixin):
+
+    def fit(self,
+            data,
+            labels,
+            clf,
+            grid_search=False,
+            targ_name='targ',
+            targ_dim_name='sample',
+            col=None):
 
         """
         Fits a classifier given class labels
@@ -14,6 +43,10 @@ class Classifiers(object):
         Args:
             data (DataArray): The data to predict on.
             labels (str | Path | GeoDataFrame): Class labels as polygon geometry.
+            clf (object): The classifier or classification pipeline.
+            grid_search (Optional[bool]): Whether to use cross-validation.
+            targ_name (Optional[str]): The target name.
+            targ_dim_name (Optional[str]): The target coordinate name.
             col (Optional[str]): The column in ``labels`` you want to assign values from.
                 If ``None``, creates a binary raster.
 
@@ -21,10 +54,31 @@ class Classifiers(object):
             ``xarray.DataArray``
 
         Example:
+            >>> import geowombat as gw
             >>> from geowombat.ml import fit
             >>>
+            >>> from sklearn_xarray import wrap
+            >>> from sklearn_xarray.preprocessing import Featurizer
+            >>> from sklearn.pipeline import Pipeline
+            >>> from sklearn.preprocessing import StandardScaler
+            >>> from sklearn.decomposition import PCA
+            >>> from sklearn.naive_bayes import GaussianNB
+            >>>
+            >>> pl = Pipeline(
+            >>>     [("featurizer", Featurizer()), 
+            >>>      ("scaler", wrap(StandardScaler)),
+            >>>      ("pca", wrap(PCA, reshapes="feature")),
+            >>>      ("cls", wrap(GaussianNB, reshapes="feature"))])
+            >>>
             >>> with gw.open('image.tif') as src:
-            >>>     X, clf = fit(src, labels)
+            >>>     X, clf = fit(src, labels, pl, grid_search=True, col='id')
+            >>>     y = clf.predict(X).unstack('sample')
+            >>>
+            >>> from sklearn.neural_network import MLPClassifier
+            >>> wrapped = wrap(MLPClassifier())
+            >>>
+            >>> with gw.open('image.tif') as src:
+            >>>     X, clf = fit(src, labels, wrapped, col='id')
             >>>     y = clf.predict(X).unstack('sample')
         """
 
@@ -43,7 +97,7 @@ class Classifiers(object):
         # TODO: what is the structure for single dates?
         if data.gw.has_time_coord:
 
-            data.coords['land_use'] = (["time", "y", "x"], labels)
+            data.coords[targ_name] = (['time', 'y', 'x'], labels)
 
             # TODO: where are we importing Stackerizer from?
             X = Stackerizer(stack_dims=('y', 'x', 'time'),
@@ -55,32 +109,26 @@ class Classifiers(object):
             # TODO: groupby as a user option?
             # Xgp = Xna.groupby('land_use').mean('sample')
 
-        # TODO: need imports
-        pl = Pipeline(
-            [
-                ("featurizer", Featurizer()),  # Stack all dimensions and variables except for sample dimension.
-                ("scaler", wrap(StandardScaler)),  # zscores , ?wrap if xarray.self required?
-                ("pca", wrap(PCA, reshapes="feature")),
-                ("cls", wrap(GaussianNB, reshapes="feature")),
-            ]
-        )
+        if grid_search:
+            clf = self.grid_search_cv(clf)
 
-        cv = CrossValidatorWrapper(
-            GroupShuffleSplit(n_splits=1, test_size=0.5), groupby=["time"]
-        )
-
-        clf = GridSearchCV(
-            pl, cv=cv, verbose=1, param_grid={"pca__n_components": [5]}
-        )
-
-        y = Target(
-            coord="land_use", transform_func=LabelEncoder().fit_transform, dim="sample")(Xna)
+        # TODO: should we be using lazy=True?
+        y = Target(coord=targ_name,
+                   transform_func=LabelEncoder().fit_transform,
+                   dim=targ_dim_name)(Xna)
 
         clf.fit(Xna, y)
 
         return X, clf
 
-    def fit_predict(self, data, labels, col=None):
+    def fit_predict(self,
+                    data,
+                    labels,
+                    clf,
+                    grid_search=False,
+                    targ_name='targ',
+                    targ_dim_name='sample',
+                    col=None):
 
         """
         Fits a classifier given class labels and predicts on a DataArray
@@ -88,6 +136,10 @@ class Classifiers(object):
         Args:
             data (DataArray): The data to predict on.
             labels (str | Path | GeoDataFrame): Class labels as polygon geometry.
+            clf (object): The classifier or classification pipeline.
+            grid_search (Optional[bool]): Whether to use cross-validation.
+            targ_name (Optional[str]): The target name.
+            targ_dim_name (Optional[str]): The target coordinate name.
             col (Optional[str]): The column in ``labels`` you want to assign values from.
                 If ``None``, creates a binary raster.
 
@@ -95,12 +147,23 @@ class Classifiers(object):
             ``xarray.DataArray``
 
         Example:
+            >>> import geowombat as gw
             >>> from geowombat.ml import fit_predict
             >>>
+            >>> from sklearn_xarray import wrap
+            >>> from sklearn.neural_network import MLPClassifier
+            >>> wrapped = wrap(MLPClassifier())
+            >>>
             >>> with gw.open('image.tif') as src:
-            >>>     y = fit_predict(src, labels)
+            >>>     y = fit_predict(src, labels, wrapped)
         """
 
-        X, clf = self.fit(data, labels, col=col)
+        X, clf = self.fit(data,
+                          labels,
+                          clf,
+                          grid_search=grid_search,
+                          targ_name=targ_name,
+                          targ_dim_name=targ_dim_name,
+                          col=col)
 
-        return clf.predict(X).unstack('sample')
+        return clf.predict(X).unstack(targ_dim_name)
