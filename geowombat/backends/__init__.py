a0fd06ca28773cc3029d8ab70a5e8a8e0ba0aaff
jgrss <graesser@bu.edu>
2020-08-20 12:30:57 +1000

    removed deprecated warnings

--- geowombat/backends/__init__.py
+++ geowombat/backends/__init__.py
@@ -1,12 +1,10 @@
 from .dask_ import Cluster
 from .xarray_ import concat, mosaic
 from .xarray_ import warp_open
-from .xarray_ import to_crs
 from .xarray_ import transform_crs
 
 __all__ = ['Cluster',
            'concat',
            'mosaic',
            'warp_open',
-           'to_crs',
            'transform_crs']
