5c412dfa525b514a6558e643cb77bd33f3aeb333
Michael Mann <mmann1123@gmail.com>
2020-08-25 15:35:40 -0400

    int only fill

--- geowombat/core/conversion.py
+++ geowombat/core/conversion.py
@@ -525,8 +525,8 @@ class Converters(object):
             row_chunks (Optional[int]): The ``dask`` row chunk size.
             col_chunks (Optional[int]): The ``dask`` column chunk size.
             src_res (Optional[tuple]: A source resolution to align to.
-            fill (Optional[int|float]): Used as fill value for all areas not covered by input geometries to ``rasterio.features.rasterize``.
-            default_value (Optional[int|float]): Used as value for all geometries, if not provided in shapes to ``rasterio.features.rasterize``.
+            fill (Optional[int]): Used as fill value for all areas not covered by input geometries to ``rasterio.features.rasterize``.
+            default_value (Optional[int]): Used as value for all geometries, if not provided in shapes to ``rasterio.features.rasterize``.
             all_touched (Optional[bool]): if True, all pixels touched by geometries will be burned in. If false, only pixels whose center 
                         is within the polygon or that are selected by Bresenham’s line algorithm will be burned in.
                         The ``all_touched`` value for ``rasterio.features.rasterize``.
