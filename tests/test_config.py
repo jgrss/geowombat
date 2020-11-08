5482320b708579428d763c655cf79619ef046890
jgrss <graesser@bu.edu>
2020-11-08 18:15:12 +1000

    added config tests

--- tests/test_config.py
+++ tests/test_config.py
@@ -32,6 +32,38 @@ class TestConfig(unittest.TestCase):
             with gw.open(l8_224078_20200518, band_names=['b1', 'b2', 'b3']) as src:
                 self.assertEqual(','.join(src.band.values.tolist()), 'b1,b2,b3')
 
+    def test_config_defaults(self):
+
+        with gw.open(l8_224078_20200518) as src:
+
+            self.assertEqual(src.gw.config['scale_factor'], 1)
+
+            for config_default in ['with_config', 'ignore_warnings']:
+                self.assertFalse(src.gw.config[config_default])
+
+            for config_default in ['sensor', 'nodata', 'ref_image', 'ref_bounds', 'ref_crs', 'ref_res', 'ref_tar', 'compress']:
+                self.assertIsNone(src.gw.config[config_default])
+
+    def test_config_set_res(self):
+
+        with gw.config.update(ref_res=100):
+
+            with gw.open(l8_224078_20200518) as src:
+                self.assertEqual(src.gw.celly, 100)
+
+        with gw.open(l8_224078_20200518) as src:
+            self.assertEqual(src.gw.celly, 30)
+
+    def test_with_config(self):
+
+        with gw.config.update():
+
+            with gw.open(l8_224078_20200518) as src:
+                self.assertTrue(src.gw.config['with_config'])
+
+        with gw.open(l8_224078_20200518) as src:
+            self.assertFalse(src.gw.config['with_config'])
+
 
 if __name__ == '__main__':
     unittest.main()
