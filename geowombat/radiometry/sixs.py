e7e65052129cd5912c7c037f5acecce51c914e9a
jgrss <graesser@bu.edu>
2021-02-09 15:52:27 +1000

    reverted zeros mask

--- geowombat/radiometry/sixs.py
+++ geowombat/radiometry/sixs.py
@@ -332,24 +332,13 @@ class SixS(Altitude):
                     .astype('float64')
 
         # Create a 'no data' mask
-        mask = sr.where((sr != src_nodata) & (band_data != src_nodata))\
-                    .count(dim='band')\
-                    .astype('uint8')
-
-        # Create a mask to check zeros
-        zmask = sr.where(sr > 0)\
+        mask = sr.where((sr != src_nodata) & (sr > 0) & (band_data != src_nodata))\
                     .count(dim='band')\
                     .astype('uint8')
 
         # Mask 'no data' values
         sr = xr.where(mask < sr.gw.nbands,
                       dst_nodata,
-                      sr.clip(0, 1))\
-                .transpose('band', 'y', 'x')
-
-        # Set zeros in all bands
-        sr = xr.where(zmask < sr.gw.nbands,
-                      0,
                       sr.clip(0, 1))\
                 .transpose('band', 'y', 'x')
 
