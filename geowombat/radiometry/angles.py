b24793b9898ee9a9619578e7c3773664b1bf6262
Jordan <graesserj@athena52.cm.cluster>
2021-02-10 15:40:35 +1000

    replaced numpy functions with xarray.ufuncs

--- geowombat/radiometry/angles.py
+++ geowombat/radiometry/angles.py
@@ -181,9 +181,9 @@ def scattering_angle(cos_sza, cos_vza, sin_sza, sin_vza, cos_raa):
         Scattering angle (in radians) as an ``xarray.DataArray``
     """
 
-    scattering_angle = np.arccos(-cos_sza * cos_vza - sin_sza * sin_vza * cos_raa)
+    scattering_angle = xr.ufuncs.arccos(-cos_sza * cos_vza - sin_sza * sin_vza * cos_raa)
 
-    return np.cos(scattering_angle) ** 2
+    return xr.ufuncs.cos(scattering_angle) ** 2
 
 
 def relative_azimuth(saa, vaa):
@@ -203,7 +203,7 @@ def relative_azimuth(saa, vaa):
     """
 
     # Relative azimuth (in radians)
-    raa = np.deg2rad(saa - vaa)
+    raa = xr.ufuncs.deg2rad(saa - vaa)
 
     # Create masks
     raa_plus = xr.where(raa >= 2.0*np.pi, 1, 0)
@@ -215,7 +215,7 @@ def relative_azimuth(saa, vaa):
     raa = xr.where(raa_plus == 1, raa - (2.0 * np.pi), raa)
     raa = xr.where(raa_minus == 1, raa + (2.0 * np.pi), raa)
 
-    return np.fabs(np.rad2deg(raa))
+    return xr.ufuncs.fabs(xr.ufuncs.rad2deg(raa))
 
 
 def get_sentinel_sensor(metadata):
@@ -270,8 +270,9 @@ def _parse_sentinel_angles(metadata, proc_angles, nodata):
     # Find the angles
     for child in root:
 
-        if child.tag[-14:] == 'Geometric_Info':
+        if child.tag.split('}')[-1] == 'Geometric_Info':
             geoinfo = child
+            break
 
     for segment in geoinfo:
 
