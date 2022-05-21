25a43cf0c2351b7eb8019017a7a327491bbe507e
jgrss <jbgraesser@gmail.com>
2022-05-21 14:01:28 -0600

    moved to src

--- setup.py
+++ setup.py
@@ -1,10 +1,7 @@
-import setuptools
 import platform
 from pathlib import Path
 from distutils.core import setup
 from distutils.extension import Extension
-import re
-from collections import defaultdict
 import subprocess
 
 try:
@@ -17,93 +14,17 @@ try:
 except:
     raise ImportError('NumPy must be installed to build GeoWombat.')
 
-# Parse the version from the module.
-# Source: https://github.com/mapbox/rasterio/blob/master/setup.py
-with open('geowombat/version.py') as f:
-    for line in f:
-
-        if line.find("__version__") >= 0:
-            version = line.split("=")[1].strip()
-            version = version.strip('"')
-            version = version.strip("'")
-
-            continue
-
-pkg_name = 'geowombat'
-maintainer = ''
-maintainer_email = ''
-description = 'GeoWombat: Utilities for geospatial data'
-git_url = 'https://github.com/jgrss/geowombat'
-download_url = '{GIT}/archive/{PKG}-{VERSION}'.format(GIT=git_url, PKG=pkg_name, VERSION=version)
-keywords = ['raster', 'satellite']
-extras = 'extra-requirements.txt'
-
-with open('README.md') as f:
-    long_description = f.read()
-
-with open('LICENSE.txt') as f:
-    license_file = f.read()
-
-with open('requirements.txt') as f:
-    required_packages = f.readlines()
 
 # Attempt to get the GDAL binary version
 try:
-
     process = subprocess.Popen(['gdalinfo', '--version'], stdout=subprocess.PIPE, stderr=None)
     gdal_version = str(process.communicate()[0]).split(',')[0].split(' ')[1].strip()
-
 except:
     gdal_version = None
 
 if gdal_version:
+    required_packages = []
     required_packages.append('GDAL=={GDAL_VERSION}\n'.format(GDAL_VERSION=gdal_version))
-
-
-def get_extra_requires(path, add_all=True):
-
-    with open(path) as fp:
-
-        extra_deps = defaultdict(set)
-
-        for k in fp:
-
-            if k.strip() and not k.startswith('#'):
-
-                tags = set()
-
-                if ':' in k:
-                    k, v = k.split(':')
-                    tags.update(vv.strip() for vv in v.split(','))
-
-                tags.add(re.split('[<=>]', k)[0])
-
-                for t in tags:
-                    extra_deps[t].add(k)
-
-        # add tag `all` at the end
-        if add_all:
-            extra_deps['all'] = set(vv for v in extra_deps.values() for vv in v)
-
-    return extra_deps
-
-
-def get_packages():
-    return setuptools.find_packages()
-
-
-def get_package_data():
-
-    return {'': ['*.md', '*.txt'],
-            'data': ['*.png'],
-            'geowombat': ['config.ini',
-                          'data/*.tif',
-                          'data/*.TIF',
-                          'data/*.gpkg',
-                          'data/*.tar.gz',
-                          'moving/*.so',
-                          'bin/*.tar.gz']}
-
 
 compile_args = ['-fopenmp']
 link_args = ['-fopenmp']
@@ -114,57 +35,38 @@ if platform.system().lower() == 'darwin':
 
 
 def get_extensions():
+    extensions = [Extension(
+        '*',
+        sources=['src/geowombat/moving/_moving.pyx'],
+        extra_compile_args=compile_args,
+        extra_link_args=link_args
+    )]
 
-    extensions = [Extension('*',
-                            sources=['geowombat/moving/_moving.pyx'],
-                            extra_compile_args=compile_args,
-                            extra_link_args=link_args)]
+    if Path('src/geowombat/moving/_test.pyx').is_file():
+        extensions += [Extension(
+            '*',
+            sources=['src/geowombat/moving/_test.pyx'],
+            extra_compile_args=compile_args,
+            extra_link_args=link_args
+        )]
 
-    if Path('geowombat/moving/_test.pyx').is_file():
-
-        extensions += [Extension('*',
-                                 sources=['geowombat/moving/_test.pyx'],
-                                 extra_compile_args=compile_args,
-                                 extra_link_args=link_args)]
-
-    if Path('geowombat/radiometry/_fusion.pyx').is_file():
-
-        extensions += [Extension('*',
-                                 sources=['geowombat/radiometry/_fusion.pyx'],
-                                 extra_compile_args=compile_args,
-                                 extra_link_args=link_args)]
+    if Path('src/geowombat/radiometry/_fusion.pyx').is_file():
+        extensions += [Extension(
+            '*',
+            sources=['src/geowombat/radiometry/_fusion.pyx'],
+            extra_compile_args=compile_args,
+            extra_link_args=link_args
+        )]
 
     return extensions
 
 
 def setup_package():
-    
-    include_dirs = [np.get_include()]
-
-    metadata = dict(name=pkg_name,
-                    maintainer=maintainer,
-                    maintainer_email=maintainer_email,
-                    description=description,
-                    license=license_file,
-                    version=version,
-                    long_description=long_description,
-                    packages=get_packages(),
-                    package_data=get_package_data(),
-                    ext_modules=cythonize(get_extensions()),
-                    zip_safe=False,
-                    keywords=' '.join(keywords),
-                    url=git_url,
-                    download_url=download_url,
-                    install_requires=required_packages,
-                    extras_require=get_extra_requires(extras),
-                    include_dirs=include_dirs,
-                    classifiers=['Intended Audience :: Science/Research',
-                                 'License :: MIT',
-                                 'Topic :: Scientific :: Remote Sensing',
-                                 'Programming Language :: Cython',
-                                 'Programming Language :: Python :: 3.6',
-                                 'Programming Language :: Python :: 3.7',
-                                 'Programming Language :: Python :: 3.8'])
+    metadata = dict(
+        ext_modules=cythonize(get_extensions()),
+        install_requires=required_packages,
+        include_dirs=[np.get_include()]
+    )
 
     setup(**metadata)
 
