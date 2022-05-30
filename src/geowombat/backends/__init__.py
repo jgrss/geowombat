173238d53da3fbeb7a899a1aec218155dea89399
jgrss <jbgraesser@gmail.com>
2022-05-29 22:48:02 -0600

    minor line reformatting

--- src/geowombat/backends/__init__.py
+++ src/geowombat/backends/__init__.py
@@ -3,8 +3,10 @@ from .xarray_ import concat, mosaic
 from .xarray_ import warp_open
 from .xarray_ import transform_crs
 
-__all__ = ['Cluster',
-           'concat',
-           'mosaic',
-           'warp_open',
-           'transform_crs']
+__all__ = [
+    'Cluster',
+    'concat',
+    'mosaic',
+    'warp_open',
+    'transform_crs'
+]
