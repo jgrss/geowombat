f03aef8e42c242ea5cdbdb40d9950f6ba9c4996c
jgrss <jbgraesser@gmail.com>
2022-07-09 16:14:49 -0600

    added failing docs lines to tests

--- tests/test_config.py
+++ tests/test_config.py
@@ -1,7 +1,9 @@
 import unittest
 
 import geowombat as gw
-from geowombat.data import l8_224077_20200518_B2, l8_224078_20200518
+from geowombat.data import l8_224078_20200518
+from geowombat.data import l8_224077_20200518_B2, l8_224077_20200518_B3, l8_224077_20200518_B4
+from geowombat.data import rgbn
 
 from pyproj import CRS
 from testfixtures import LogCapture
@@ -85,6 +87,25 @@ class TestConfig(unittest.TestCase):
                 self.assertTrue(src.crs, 8858)
                 self.assertTrue(test_crs, src.gw.crs_to_pyproj)
                 self.assertTrue(src.res, (100.0, 100.0))
+
+    def test_multiple_crs(self):
+        test_crs = CRS.from_user_input('epsg:32621')
+        filenames = [l8_224078_20200518, l8_224078_20200518]
+        with gw.config.update(ref_image=l8_224077_20200518_B2):
+            with gw.open(
+                filenames,
+                band_names=['blue', 'green', 'red'],
+                time_names=['t1', 't2']
+            ) as src:
+                self.assertTrue(src.crs, 32621)
+                self.assertTrue(test_crs, src.gw.crs_to_pyproj)
+
+    def test_unique_crs(self):
+        proj4 = "+proj=aea +lat_1=20 +lat_2=60 +lat_0=40 +lon_0=-96 +x_0=0 +y_0=0 +datum=NAD83 +units=m +no_defs"
+        with gw.open(rgbn) as src:
+            dst = src.gw.transform_crs(proj4, dst_res=(10, 10), resampling='bilinear')
+            self.assertEqual(dst.crs, proj4)
+            self.assertEqual(dst.gw.crs_to_pyproj, proj4)
 
     def test_warnings_ignore(self):
         with LogCapture() as log:
