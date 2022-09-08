9006f2ea0b62f1a18321c902915a14a9a2437624
jgrss <jbgraesser@gmail.com>
2022-09-08 11:41:51 -0600

    minor

--- tests/test_clip.py
+++ tests/test_clip.py
@@ -1,7 +1,7 @@
 import unittest
 
 import geowombat as gw
-from geowombat.data import l8_224077_20200518_B2, l8_224077_20200518_B3,l8_224078_20200518_polygons
+from geowombat.data import l8_224077_20200518_B2, l8_224077_20200518_B3, l8_224078_20200518_polygons
 
 
 class TestClip(unittest.TestCase):
