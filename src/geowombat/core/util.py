b30aa7fe7e10e5b70524109418335aadf407cdff
jgrss <jbgraesser@gmail.com>
2022-09-13 16:18:17 -0600

    changed default band chunk size

--- src/geowombat/core/util.py
+++ src/geowombat/core/util.py
@@ -331,14 +331,12 @@ class Chunks(object):
 
     @staticmethod
     def check_chunksize(chunksize, output='3d'):
-
         chunk_len = len(chunksize)
         output_len = int(output[0])
 
         if chunk_len != output_len:
-
             if (chunk_len == 2) and (output_len == 3):
-                return (1,) + chunksize
+                return (-1,) + chunksize
             elif (chunk_len == 3) and (output_len == 2):
                 return chunksize[1:]
 
