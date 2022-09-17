b409461c3bf8e5bece189027c4b762ab0d5f1808
jgrss <jbgraesser@gmail.com>
2022-09-17 17:18:26 -0600

    Black formatting

--- tests/test_write.py
+++ tests/test_write.py
@@ -3,7 +3,11 @@ import tempfile
 from pathlib import Path
 
 import geowombat as gw
-from geowombat.data import l8_224078_20200518, l8_224077_20200518_B2, l8_224077_20200518_B3
+from geowombat.data import (
+    l8_224078_20200518,
+    l8_224077_20200518_B2,
+    l8_224077_20200518_B3,
+)
 import dask
 
 
@@ -14,14 +18,10 @@ class TestConfig(unittest.TestCase):
             out_path = Path(tmp) / 'test.nc'
             with gw.open(l8_224078_20200518, band_names=bands) as src:
                 (
-                    src
-                    .fillna(32768)
-                    .assign_attrs(nodatavals=(32768,))
+                    src.fillna(32768)
+                    .gw.assign_nodata_attrs(32768)
                     .astype('uint16')
-                    .gw.to_netcdf(
-                        filename=out_path,
-                        overwrite=True
-                    )
+                    .gw.to_netcdf(filename=out_path, overwrite=True)
                 )
 
     def test_to_raster(self):
@@ -29,16 +29,15 @@ class TestConfig(unittest.TestCase):
             out_path = Path(tmp) / 'test.tif'
             with gw.open(l8_224078_20200518) as src:
                 (
-                    src
-                    .fillna(32768)
-                    .assign_attrs(nodatavals=(32768,))
+                    src.fillna(32768)
+                    .gw.assign_nodata_attrs(32768)
                     .astype('uint16')
                     .gw.to_raster(
                         filename=out_path,
                         overwrite=True,
                         tags={'TEST_METADATA': 'TEST_VALUE'},
                         compression='lzw',
-                        num_workers=2
+                        num_workers=2,
                     )
                 )
                 with gw.open(out_path) as tmp_src:
@@ -52,17 +51,14 @@ class TestConfig(unittest.TestCase):
             with gw.open(
                 [l8_224077_20200518_B2, l8_224077_20200518_B3],
                 stack_dim='band',
-                band_names=['B2', 'B3']
+                band_names=['B2', 'B3'],
             ) as src:
                 # Xarray drops attributes
                 attrs = src.attrs.copy()
                 # Apply operations on the DataArray
                 src = src * 10.0
                 # Write the data to a GeoTiff
-                src.assign_attrs(**attrs).gw.to_raster(
-                    out_path,
-                    overwrite=True
-                )
+                src.assign_attrs(**attrs).gw.to_raster(out_path, overwrite=True)
                 self.assertTrue(out_path.is_file())
                 with gw.open(out_path, band_names=['B2', 'B3']) as tmp_src:
                     self.assertTrue(src.equals(tmp_src))
@@ -72,16 +68,15 @@ class TestConfig(unittest.TestCase):
             out_path = Path(tmp) / 'test.tif'
             with gw.open(l8_224078_20200518) as src:
                 (
-                    src
-                    .fillna(32768)
-                    .assign_attrs(nodatavals=(32768,))
+                    src.fillna(32768)
+                    .gw.assign_nodata_attrs(32768)
                     .astype('uint16')
                     .gw.save(
                         filename=out_path,
                         overwrite=True,
                         tags={'TEST_METADATA': 'TEST_VALUE'},
                         compression='lzw',
-                        num_workers=2
+                        num_workers=2,
                     )
                 )
                 with gw.open(out_path) as tmp_src:
@@ -93,12 +88,7 @@ class TestConfig(unittest.TestCase):
         with tempfile.TemporaryDirectory() as tmp:
             out_path = Path(tmp) / 'test.tif'
             with gw.open(l8_224078_20200518) as src:
-                src = (
-                    src
-                    .fillna(32768)
-                    .assign_attrs(nodatavals=(32768,))
-                    .astype('uint16')
-                )
+                src = src.fillna(32768).gw.assign_nodata_attrs(32768).astype('uint16')
                 tasks = [
                     gw.save(
                         src,
@@ -107,7 +97,7 @@ class TestConfig(unittest.TestCase):
                         compression='lzw',
                         num_workers=2,
                         compute=False,
-                        overwrite=True
+                        overwrite=True,
                     )
                 ]
             dask.compute(tasks, num_workers=2)
