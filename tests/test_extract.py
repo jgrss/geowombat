e30f492aa2bb17401fa64f4283300ba774f57fe1
mmann1123 <mmann1123@gmail.com>
2024-03-07 15:08:39 -0500

    adding ray extract tests

--- tests/test_extract.py
+++ tests/test_extract.py
@@ -11,14 +11,21 @@ from geowombat.data import (
 import numpy as np
 import geopandas as gpd
 from sklearn.preprocessing import LabelEncoder
+from pandas.testing import assert_frame_equal
 
+try:
+    import ray
+
+    RAY_AVAILABLE = True
+except ImportError:
+    RAY_AVAILABLE = False
 
 aoi = gpd.read_file(l8_224078_20200518_points)
-aoi['id'] = LabelEncoder().fit_transform(aoi.name)
-aoi = aoi.drop(columns=['name'])
+aoi["id"] = LabelEncoder().fit_transform(aoi.name)
+aoi = aoi.drop(columns=["name"])
 
 l8_224078_20200518_B2_values = np.array(
-    [7966, 8030, 7561, 8302, 8277, 7398], dtype='float64'
+    [7966, 8030, 7561, 8302, 8277, 7398], dtype="float64"
 )
 
 l8_224078_20200518_values = np.array(
@@ -27,55 +34,116 @@ l8_224078_20200518_values = np.array(
         [7326, 7490, 6874, 8202, 7982, 6711],
         [6254, 8080, 6106, 8111, 7341, 6007],
     ],
-    dtype='float64',
+    dtype="float64",
 )
 
 
 class TestExtract(unittest.TestCase):
     def test_single_image_single_band(self):
         with gw.open(l8_224078_20200518_B2) as src:
-            df = gw.extract(src, aoi, band_names=['blue'])
+            df = gw.extract(src, aoi, band_names=["blue"])
 
-        self.assertTrue(np.allclose(df.blue.values, l8_224078_20200518_B2_values))
+        self.assertTrue(
+            np.allclose(df.blue.values, l8_224078_20200518_B2_values)
+        )
 
     def test_single_image_multi_band(self):
         with gw.open(l8_224078_20200518) as src:
-            df = gw.extract(src, aoi, band_names=['blue', 'green', 'red'])
+            df = gw.extract(src, aoi, band_names=["blue", "green", "red"])
 
-        self.assertTrue(np.allclose(df.blue.values, l8_224078_20200518_values[0, :]))
-        self.assertTrue(np.allclose(df.green.values, l8_224078_20200518_values[1, :]))
-        self.assertTrue(np.allclose(df.red.values, l8_224078_20200518_values[2, :]))
+        self.assertTrue(
+            np.allclose(df.blue.values, l8_224078_20200518_values[0, :])
+        )
+        self.assertTrue(
+            np.allclose(df.green.values, l8_224078_20200518_values[1, :])
+        )
+        self.assertTrue(
+            np.allclose(df.red.values, l8_224078_20200518_values[2, :])
+        )
 
     def test_multi_image_single_band(self):
         with gw.open([l8_224078_20200518_B2, l8_224078_20200518_B2]) as src:
-            df = gw.extract(src, aoi, band_names=['blue'])
+            df = gw.extract(src, aoi, band_names=["blue"])
 
-        self.assertTrue(np.allclose(df.t1_blue.values, l8_224078_20200518_B2_values))
-        self.assertTrue(np.allclose(df.t2_blue.values, l8_224078_20200518_B2_values))
+        self.assertTrue(
+            np.allclose(df.t1_blue.values, l8_224078_20200518_B2_values)
+        )
+        self.assertTrue(
+            np.allclose(df.t2_blue.values, l8_224078_20200518_B2_values)
+        )
+
+    @unittest.skipUnless(
+        RAY_AVAILABLE, "Ray is not available - skipping ray extract"
+    )
+    def test_clients_all_same_df(self):
+        with gw.open(l8_224078_20200518_B2) as src:
+            ray.init()
+            df_ray = gw.extract(
+                src,
+                aoi,
+                band_names=["blue", "green", "red"],
+                use_ray_client=True,
+            )
+            ray.shutdown()
+            df_none = gw.extract(
+                src,
+                aoi,
+                band_names=["blue", "green", "red"],
+                use_client=False,
+            )
+            df_true = gw.extract(
+                src,
+                aoi,
+                band_names=["blue", "green", "red"],
+                use_client=True,
+            )
+        self.assertTrue(assert_frame_equal(df_ray, df_none))
+        self.assertTrue(assert_frame_equal(df_ray, df_true))
 
     def test_multi_image_multi_band(self):
         with gw.open([l8_224078_20200518, l8_224078_20200518]) as src:
-            df = gw.extract(src, aoi, band_names=['blue', 'green', 'red'])
+            df = gw.extract(src, aoi, band_names=["blue", "green", "red"])
 
-        self.assertTrue(np.allclose(df.t1_blue.values, l8_224078_20200518_values[0, :]))
+        self.assertTrue(
+            np.allclose(df.t1_blue.values, l8_224078_20200518_values[0, :])
+        )
         self.assertTrue(
             np.allclose(df.t1_green.values, l8_224078_20200518_values[1, :])
         )
-        self.assertTrue(np.allclose(df.t1_red.values, l8_224078_20200518_values[2, :]))
-        self.assertTrue(np.allclose(df.t2_blue.values, l8_224078_20200518_values[0, :]))
+        self.assertTrue(
+            np.allclose(df.t1_red.values, l8_224078_20200518_values[2, :])
+        )
+        self.assertTrue(
+            np.allclose(df.t2_blue.values, l8_224078_20200518_values[0, :])
+        )
         self.assertTrue(
             np.allclose(df.t2_green.values, l8_224078_20200518_values[1, :])
         )
-        self.assertTrue(np.allclose(df.t2_red.values, l8_224078_20200518_values[2, :]))
+        self.assertTrue(
+            np.allclose(df.t2_red.values, l8_224078_20200518_values[2, :])
+        )
 
     def test_points(self):
         with gw.open(l8_224078_20200518_B2) as src:
-            df = gw.extract(src, l8_224078_20200518_polygons, band_names=['blue'])
+            df = gw.extract(
+                src, l8_224078_20200518_polygons, band_names=["blue"]
+            )
             self.assertTrue(
                 np.allclose(
                     df.blue.values[:10],
                     np.array(
-                        [7994, 8017, 8008, 8008, 8018, 8007, 7991, 7993, 7981, 7991]
+                        [
+                            7994,
+                            8017,
+                            8008,
+                            8008,
+                            8018,
+                            8007,
+                            7991,
+                            7993,
+                            7981,
+                            7991,
+                        ]
                     ),
                 )
             )
@@ -119,5 +187,5 @@ class TestExtract(unittest.TestCase):
             )
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     unittest.main()
