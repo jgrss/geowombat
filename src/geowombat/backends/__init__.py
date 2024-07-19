529608926d28aaa835ba15c4d10c4117d23f4298
mmann1123 <mmann1123@gmail.com>
2024-07-19 13:22:41 -0400

    Revert "add error if mosaic/stack and change res crs"
    This reverts commit d8101fabcbe346076ab982ce19f3d83dc0c342da.

--- src/geowombat/backends/__init__.py
+++ src/geowombat/backends/__init__.py
@@ -1,17 +1,4 @@
 from .dask_ import Cluster
-from .xarray_ import (
-    concat,
-    mosaic,
-    transform_crs,
-    warp_open,
-    _check_config_globals,
-)
+from .xarray_ import concat, mosaic, transform_crs, warp_open
 
-__all__ = [
-    "Cluster",
-    "concat",
-    "mosaic",
-    "warp_open",
-    "transform_crs",
-    "_check_config_globals",
-]
+__all__ = ['Cluster', 'concat', 'mosaic', 'warp_open', 'transform_crs']
